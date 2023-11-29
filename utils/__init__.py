71b41f3ec3b5342cc80768b492986b69b6e78999
Daniel Altunay <daniel.altunay@decathlon.com>
2023-11-29 11:14:06 +0100

    format

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,6 @@
 from utils.logging import set_logger
-from utils.misc import base64_to_img, generate_logo_link, show_logos, show_source_code
+from utils.misc import (base64_to_img, generate_logo_link, show_logos,
+                        show_source_code)
 from utils.secrets import load_secrets
 
 __all__ = [
