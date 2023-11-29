386856f753900d1a2378f4fab276aa3cea748222
Daniel Altunay <daniel.altunay@decathlon.com>
2023-11-29 15:23:39 +0100

    rename lakera manager

--- app/sidebar/sidebar.py
+++ app/sidebar/sidebar.py
@@ -1,16 +1,15 @@
 import streamlit as st
 
-from app.sidebar import LakeraAPIManager  # , LanguageManager, ModelAPIManager
+from app.sidebar import \
+    LakeraGuardAPIManager  # , LanguageManager, ModelAPIManager
 
 
 class Sidebar:
     def __init__(self):
-        # self.language_manager = LanguageManager()
-        # self.model_api_manager = ModelAPIManager()
-        self.lakera_api_manager = LakeraAPIManager()
+        self.lakera_guard_api_manager = st.session_state.setdefault(
+            "lakera_guard_api_manager", LakeraGuardAPIManager()
+        )
 
     def main(self):
         with st.sidebar:
-            # self.language_manager.main()
-            # self.model_api_manager.main()
-            self.lakera_api_manager.main()
+            self.lakera_guard_api_manager.main()
