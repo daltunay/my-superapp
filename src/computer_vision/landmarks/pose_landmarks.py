7c95c1a56b126ea8c6bc89e2eef11cfeb427535b
Daniel Altunay <daniel.altunay@decathlon.com>
2023-12-04 13:41:37 +0100

    unsmooth

--- src/computer_vision/landmarks/pose_landmarks.py
+++ src/computer_vision/landmarks/pose_landmarks.py
@@ -17,7 +17,7 @@ class PoseLandmarkerApp(BaseLandmarkerApp):
         return mp.solutions.pose.Pose(
             static_image_mode=False,
             model_complexity=1,
-            smooth_landmarks=True,
+            smooth_landmarks=False,
             enable_segmentation=False,
             min_detection_confidence=0.5,
             min_tracking_confidence=0.5,
