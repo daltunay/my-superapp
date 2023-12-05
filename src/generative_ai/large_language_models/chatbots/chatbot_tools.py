13d2eb764b4fac4286a09366a99f6a7405bd62ad
Daniel Altunay <daniel.altunay@decathlon.com>
2023-12-05 12:32:21 +0100

    fix

--- src/generative_ai/large_language_models/chatbots/chatbot_tools.py
+++ src/generative_ai/large_language_models/chatbots/chatbot_tools.py
@@ -63,12 +63,9 @@ class ChatbotTools(Chatbot):
         )
         agent = self.update_human_msg_prompt_template(
             agent=agent,
-            text_to_add="\nThe final answer must come in {language}.",
+            text_to_add="\nThe final answer must come in {language}, "
+            "in the format of a markdown code snippet of a json blob with a single action.",
             input_variable_to_add="language",
-        )
-        agent = self.update_human_msg_prompt_template(
-            agent=agent,
-            text_to_add="\nThe final answer must come in a markdown code snippet of a json blob with a single action.",
         )
         return agent
 
