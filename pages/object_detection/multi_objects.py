78a9a0ff19f9eb5ee619e478516d2ada215f0d63
Daniel Altunay <daniel.altunay@decathlon.com>
2023-12-06 16:14:57 +0100

    add app

--- pages/object_detection/multi_objects.py
+++ pages/object_detection/multi_objects.py
@@ -1,4 +1,17 @@
+import streamlit as st
+
 import utils
+from src.computer_vision.object_detection.multi_objects import MultiObjectsDetectionApp
 
 loader = utils.PageConfigLoader(__file__)
 loader.set_page_config(globals())
+
+logger = utils.CustomLogger(__file__)
+
+st_ss = st.session_state
+
+
+def main():
+    utils.show_source_code(path="src/computer_vision/object_detection/multi_objects.py")
+
+    st_ss.setdefault("multi_objects_detection_app", MultiObjectsDetectionApp()).stream()
