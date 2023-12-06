ee8dfa87525dcdb5a2aa61be49a1fcf7d4ebad90
Daniel Altunay <daniel.altunay@decathlon.com>
2023-12-06 17:21:26 +0100

    format

--- src/computer_vision/object_detection/__init__.py
+++ src/computer_vision/object_detection/__init__.py
@@ -1,5 +1,6 @@
-from src.computer_vision.object_detection.face_detection import FaceDetectionApp
-from src.computer_vision.object_detection.multi_objects import MultiObjectsDetectionApp
-
+from src.computer_vision.object_detection.face_detection import \
+    FaceDetectionApp
+from src.computer_vision.object_detection.multi_objects import \
+    MultiObjectsDetectionApp
 
 __all__ = ["FaceDetectionApp", "MultiObjectsDetectionApp"]
