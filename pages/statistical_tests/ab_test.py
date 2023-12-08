98b980f72331c69129795dd7ca8c24474c166436
Daniel Altunay <daniel.altunay@decathlon.com>
2023-12-08 18:23:14 +0100

    format

--- pages/statistical_tests/ab_test.py
+++ pages/statistical_tests/ab_test.py
@@ -8,18 +8,51 @@ loader.set_page_config(globals())
 
 
 def main():
-    st.title("A/B Testing Tool")
-
-    a_conversions = st.number_input("Conversions for Group A", min_value=0, step=1)
-    a_visitors = st.number_input("Total visitors for Group A", min_value=1, step=1)
-    b_conversions = st.number_input("Conversions for Group B", min_value=0, step=1)
-    b_visitors = st.number_input("Total visitors for Group B", min_value=1, step=1)
+    a_col, b_col = st.columns(2)
+    with a_col:
+        st.subheader("Group A")
+        a_visitors = st.number_input(
+            "Total visitors for Group A",
+            min_value=1,
+            value=1000,
+            step=1,
+        )
+        a_conversions = st.number_input(
+            "Conversions for Group A",
+            min_value=0,
+            max_value=a_visitors,
+            value=50,
+            step=1,
+        )
+    with b_col:
+        st.subheader("Group B")
+        b_visitors = st.number_input(
+            "Total visitors for Group B",
+            min_value=1,
+            value=200,
+            step=1,
+        )
+        b_conversions = st.number_input(
+            "Conversions for Group B",
+            min_value=0,
+            max_value=b_visitors,
+            value=35,
+            step=1,
+        )
     confidence = st.slider(
-        "Confidence level (%)", min_value=1, max_value=99, value=95, step=1
+        "Confidence level (%)",
+        min_value=1,
+        max_value=99,
+        value=95,
+        step=1,
     )
 
     ab_testing = ABTesting(
-        a_conversions, a_visitors, b_conversions, b_visitors, confidence
+        a_conversions,
+        a_visitors,
+        b_conversions,
+        b_visitors,
+        confidence,
     )
 
     result = ab_testing.perform_ab_test()
