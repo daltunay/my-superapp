2f8d5e06d2e17c9cd867a541ca9fe1bec12aa2dd
Daniel Altunay <daniel.altunay@decathlon.com>
2023-12-10 12:49:34 +0100

    add chi2 test

--- pages/statistical_tests/chi2_test.py
+++ pages/statistical_tests/chi2_test.py
@@ -1,4 +1,46 @@
+import streamlit as st
+import pandas as pd
+
 import utils
+from src.statistics.statistical_tests import Chi2Testing
 
 loader = utils.PageConfigLoader(__file__)
 loader.set_page_config(globals())
+
+
+def main():
+    st.header("Data", divider="gray")
+    observed_template = pd.DataFrame(
+        data=[[0, 0], [0, 0]],
+        index=["Group A", "Group B"],
+        columns=["Category 1", "Category 2"],
+    )
+    observed = st.data_editor(data=observed_template, use_container_width=True)
+
+    st.header("Settings", divider="gray")
+    settings_container = st.container(border=True)
+    confidence_col, alpha_col = settings_container.columns(2)
+    confidence = confidence_col.columns([0.15, 1, 0.15])[1].select_slider(
+        "Confidence level",
+        options=[0.9, 0.95, 0.99],
+        value=0.95,
+        key="ab_test.confidence",
+        format_func=lambda x: f"{100*x}%",
+        on_change=utils.update_slider_callback,
+        kwargs={"updated": "ab_test.confidence", "to_update": "ab_test.alpha"},
+    )
+    alpha = alpha_col.columns([0.15, 1, 0.15])[1].select_slider(
+        "Alpha value",
+        options=[0.01, 0.05, 0.1],
+        value=0.05,
+        key="ab_test.alpha",
+        format_func=lambda x: f"{100*x}%",
+        on_change=utils.update_slider_callback,
+        kwargs={"updated": "ab_test.alpha", "to_update": "ab_test.confidence"},
+    )
+
+    chi2_testing = Chi2Testing(observed, alpha)
+
+    st.header("Results", divider="gray")
+    result = chi2_testing.perform_chi2_test()
+    st.json(result)
---
+++ src/statistics/statistical_tests/chi_squared.py
@@ -0,0 +1,38 @@
+import typing as t
+import streamlit as st
+from scipy.stats import chi2_contingency
+
+
+class Chi2Testing:
+    def __init__(
+        self,
+        observed: t.List[t.List[int]],
+        alpha: float,
+    ):
+        self.observed = observed
+        self.alpha = alpha
+
+    @staticmethod
+    @st.cache_data(show_spinner=False)
+    def chi2_test(
+        observed: t.List[t.List[int]],
+    ) -> t.Tuple[float, float, int, t.List[t.List[float]]]:
+        chi2, p_value, dof, expected = chi2_contingency(observed)
+        return chi2, p_value, dof, expected
+
+    @staticmethod
+    @st.cache_data(show_spinner=False)
+    def is_statistically_significant(p_value: float, alpha: float) -> bool:
+        return p_value < alpha
+
+    def perform_chi2_test(self) -> t.Dict[str, t.Any]:
+        chi2, p_value, dof, expected = self.chi2_test(self.observed)
+        is_significant = self.is_statistically_significant(p_value, self.alpha)
+
+        return {
+            "chi2_statistic": chi2,
+            "p_value": p_value,
+            "degrees_of_freedom": dof,
+            "expected_frequencies": expected,
+            "is_significant": is_significant,
+        }
